5ed021022c6ba028dad19db6ff8388f3b35688a0
Laurent Saidah <lsaidah@gmail.com>
2020-06-01 12:26:26 -0500

    Starting filling out children method

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -3,7 +3,7 @@ require_relative 'tic_tac_toe'
 class TicTacToeNode
 
   attr_reader :board, :next_mover_mark, :prev_move_pos
-  
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
     @next_mover_mark = next_mover_mark
@@ -20,5 +20,17 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+
+    @board.rows.each do |row|
+      row.each do |space|
+        
+          @next_mover_mark == :x ? mark = :o : mark = :x
+         p mark
+         child_node = TicTacToeNode.new(@board, mark, space)
+         @next_mover_mark = mark
+      
+      end
+    end
   end
 end
+

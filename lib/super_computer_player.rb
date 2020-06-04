3afeef9f10612b7ad17407e4ebd2b69d0c9fa668
Laurent Saidah <lsaidah@gmail.com>
2020-06-04 11:14:11 -0500

    filling out move method

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -2,6 +2,15 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    t_node = TicTacToeNode.new(game.board, mark)
+    winning_nodes = t_node.children.select {|child| child.winning_node?(mark)}
+    return winning_nodes.first.prev_move_pos unless winning_nodes.empty?
+    non_loosing_moves = t_node.children.select {|child| !child.losing_node?(mark)}
+    return non_loosing_moves.first.prev_move_pos unless non_loosing_moves.empty?
+
+
+    raise "No available moves"
+
   end
 end
 

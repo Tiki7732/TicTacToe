fb9ac2f1971d957036f48ae5cee6a205d6b6f0d3
Laurent Saidah <lsaidah@gmail.com>
2020-06-04 12:03:40 -0500

    finished the move method

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -2,16 +2,19 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
-    t_node = TicTacToeNode.new(game.board, mark)
-    winning_nodes = t_node.children.select {|child| child.winning_node?(mark)}
-    return winning_nodes.first.prev_move_pos unless winning_nodes.empty?
-    non_loosing_moves = t_node.children.select {|child| !child.losing_node?(mark)}
-    return non_loosing_moves.first.prev_move_pos unless non_loosing_moves.empty?
+    node = TicTacToeNode.new(game.board, mark)
+    moves = node.children.shuffle
+    winning_moves = moves.select {|child| child.winning_node?(mark)}
 
 
+    return winning_moves.first.prev_move_pos unless winning_moves.empty?
+
+    blocking_moves = moves.select {|child| !child.losing_node?(mark)}
+    return blocking_moves.first.prev_move_pos unless blocking_moves.empty?
     raise "No available moves"
 
   end
+ 
 end
 
 if __FILE__ == $PROGRAM_NAME
